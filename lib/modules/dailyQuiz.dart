2131c5bdafad6a9878fb3d1431cbda90f958688e
malego290704 <malego290704@gmail.com>
2020-07-11 23:38:56 +0700

    Add files via upload

---
+++ lib/modules/dailyQuiz.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'dailyQuizElements.dart';
+import 'dailyQuizData.dart';
+
+class DailyQuizInterface extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    quizDatabaseGenerator();
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(getQuizPagePadding()),
+          child: Column(
+            children: <Widget>[
+              Text('Hãy thử trả lời một câu đố', style: Theme.of(context).textTheme.headline5,),
+              Align(
+                alignment: Alignment.center,
+                child: RaisedButton(
+                  child: Text('Thực hiện câu đố', style: Theme.of(context).textTheme.headline4,),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => QuizLoaderPage(quizData: getSingleRandomQuizData(), quizAnswered: false,)),
+                    );
+                  },
+                  elevation: 8,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+    //return QuizLoaderPage(quizData: getSingleRandomQuizData(),);
+  }
+}
